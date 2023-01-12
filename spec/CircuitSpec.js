3cfa43f0df89aa6a045450dfa3128906c3a77168
CheddarBobInc <112483721+CheddarBobInc@users.noreply.github.com>
2023-01-12 10:13:51 +0000

    trying to setup circuit initialisation
    need to test an array length

--- spec/CircuitSpec.js
+++ spec/CircuitSpec.js
@@ -1,24 +1,25 @@
-// const { Battery } = require("../src/Battery");
-// const { Circuit } = require("../src/Circuit");
-// const { Wire } = require("../src/Wire");
-// const { Connection } = require("../src/Connection");
-// const {
-//   DEFAULT_CURRENT,
-//   DEFAULT_RESISTANCE,
-//   DEFAULT_POTENTIALDIFFERENCE,
-//   DEFAULT_BATTERY_POTENTIALDIFFERENCE,
-// } = require("../src/properties");
+const { Battery } = require("../src/Battery");
+const { Circuit } = require("../src/Circuit");
+const { Wire } = require("../src/Wire");
+const { Connection } = require("../src/Connection");
+const {
+  DEFAULT_CURRENT,
+  DEFAULT_RESISTANCE,
+  DEFAULT_POTENTIALDIFFERENCE,
+  DEFAULT_BATTERY_POTENTIALDIFFERENCE,
+} = require("../src/properties");
 
-// describe("Circuit", () => {
-//     let circuit;
+describe("Circuit", () => {
+    let circuit;
   
-//     beforeEach(() => {
-//       circuit = new Circuit();
-//     });
+    beforeEach(() => {
+      circuit = new Circuit();
+    });
 
-//     it("must have at least two connections", () => {
-//         expect(circuit.isComplete).toEqual(false);
-//         circuit = new Circuit([0, 1, 3]);
-//         expect(circuit.isComplete).toEqual(true);
-//     })
-// });
+    it("has 4 connectors", () => {
+        circuit.initialiseCircuit();
+        circuitConnectors = circuit.connectors;
+        numOfConnectors = circuitConnectors.length;
+        expect(numOfConnectors).toEqual(4);
+    })
+});
--- src/Circuit.js
+++ src/Circuit.js
@@ -4,21 +4,29 @@ const { Battery } = require("./Battery");
 const { Connection } = require("./Connection");
 const { Connector } = require("./Connector");
 
-let connections = [];
 let components = [];
-let wire = new Wire();
-wire.positiveConnection.coordinate = 00;
-let connector = new Connector();
-connector.up.coordinate = 00;
 
 class Circuit {
-  constructor(connections = [], components = []) {
-    this.connections = connections;
+  constructor(numOfConnectors = 4, components = []) {
+    this.numOfConnectors = numOfConnectors;
     this.components = components;
     this.hasMinimumConnections = this.connections.length > 4 ? true : false;
     this.hasMinimumComponents = this.components.length > 2 ? true : false;
-    this.isComplete =
-      this.hasMinimumConnections && this.hasMinimumComponents ? true : false;
+    this.isComplete = false;
+    this.hasMinimumConnections && this.hasMinimumComponents ? true : false;
+    this.connectors = [];
+  }
+
+  initialiseCircuit(numOfConnectors) {
+    // creates connectors
+    for (let i = 0; i < numOfConnectors; i++) {
+      connectors[i] = new Connector();
+    }
+
+    // gives connectors connections there coordinates
+    // for (let i = 0; i < connectors.length; i++) {
+    //   connectors[i].up
+    // }
   }
 }
 
