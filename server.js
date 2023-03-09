79144d58914c385482cfe77911fa287d10384e67
CheddarBobInc <112483721+CheddarBobInc@users.noreply.github.com>
2023-03-09 10:08:58 +0000

    attempted to connect mongodb atlas to project

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://cpalmer:OvBVJHQGMRVMR7yh@cluster0.x1qjsou.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 const express = require('express');
 const app = express();
 const port = 5000;
