fa66f5528bcb0d09919e5739cb25493a39089c0f
CheddarBobInc <112483721+CheddarBobInc@users.noreply.github.com>
2023-03-23 09:54:02 +0000

    fixed typo

--- spec/GraphSpec.js
+++ spec/GraphSpec.js
@@ -14,39 +14,39 @@ describe("Graph", () => {
     graph = new Graph();
   });
 
-  it("has list of verticies", () => {
-    expect(graph.verticies).toBeInstanceOf(LinkedList);
+  it("has list of vertices", () => {
+    expect(graph.vertices).toBeInstanceOf(LinkedList);
   });
 
   it("has length 0", () => {
     expect(graph.length).toEqual(0);
   });
 
-  it("has method addVertex that adds a vertex to verticies", () => {
+  it("has method addVertex that adds a vertex to vertices", () => {
     graph.addVertex(new Wire());
     graph.addVertex(new Battery());
     graph.addVertex(new Lever());
 
-    expect(graph.verticies.getAt(0).index).toEqual(0);
-    expect(graph.verticies.getAt(0).component).toBeInstanceOf(Wire);
-    expect(graph.verticies.getAt(0).listOfConnections).toBeInstanceOf(
+    expect(graph.vertices.getAt(0).index).toEqual(0);
+    expect(graph.vertices.getAt(0).component).toBeInstanceOf(Wire);
+    expect(graph.vertices.getAt(0).listOfConnections).toBeInstanceOf(
       LinkedList
     );
 
-    expect(graph.verticies.getAt(1).index).toEqual(1);
-    expect(graph.verticies.getAt(1).component).toBeInstanceOf(Battery);
-    expect(graph.verticies.getAt(1).listOfConnections).toBeInstanceOf(
+    expect(graph.vertices.getAt(1).index).toEqual(1);
+    expect(graph.vertices.getAt(1).component).toBeInstanceOf(Battery);
+    expect(graph.vertices.getAt(1).listOfConnections).toBeInstanceOf(
       LinkedList
     );
 
-    expect(graph.verticies.getAt(2).index).toEqual(2);
-    expect(graph.verticies.getAt(2).component).toBeInstanceOf(Lever);
-    expect(graph.verticies.getAt(2).listOfConnections).toBeInstanceOf(
+    expect(graph.vertices.getAt(2).index).toEqual(2);
+    expect(graph.vertices.getAt(2).component).toBeInstanceOf(Lever);
+    expect(graph.vertices.getAt(2).listOfConnections).toBeInstanceOf(
       LinkedList
     );
   });
 
-  it("has method addEdge that adds edge between two verticies", () => {
+  it("has method addEdge that adds edge between two vertices", () => {
     graph.addVertex(new Wire());
     graph.addVertex(new Battery());
     graph.addVertex(new Lever());
@@ -56,13 +56,13 @@ describe("Graph", () => {
     graph.addEdge(1, 2);
     graph.addEdge(1, 3);
 
-    expect(graph.verticies.getAt(0).listOfConnections.getAt(0)).toEqual(1);
-    expect(graph.verticies.getAt(0).listOfConnections.getAt(1)).toEqual(2);
-    expect(graph.verticies.getAt(1).listOfConnections.getAt(0)).toEqual(2);
-    expect(graph.verticies.getAt(1).listOfConnections.getAt(1)).toEqual(undefined);
+    expect(graph.vertices.getAt(0).listOfConnections.getAt(0)).toEqual(1);
+    expect(graph.vertices.getAt(0).listOfConnections.getAt(1)).toEqual(2);
+    expect(graph.vertices.getAt(1).listOfConnections.getAt(0)).toEqual(2);
+    expect(graph.vertices.getAt(1).listOfConnections.getAt(1)).toEqual(undefined);
   });
 
-  it("has method removeEdge that removes an edge between two verticies", () => {
+  it("has method removeEdge that removes an edge between two vertices", () => {
     graph.addVertex(new Wire());
     graph.addVertex(new Battery());
     graph.addVertex(new Lever());
@@ -72,9 +72,9 @@ describe("Graph", () => {
     graph.addEdge(1, 2);
     graph.addEdge(1, 3);
 
-    expect(graph.verticies.getAt(0).listOfConnections.getAt(1)).toEqual(2);
+    expect(graph.vertices.getAt(0).listOfConnections.getAt(1)).toEqual(2);
     graph.removeEdge(0, 2);
-    expect(graph.verticies.getAt(0).listOfConnections.getAt(1)).toEqual(undefined);
+    expect(graph.vertices.getAt(0).listOfConnections.getAt(1)).toEqual(undefined);
   });
 
   it("has method removeVertex removes vertex from graph", () => {
@@ -88,14 +88,14 @@ describe("Graph", () => {
     graph.addEdge(1, 2);
     graph.addEdge(1, 3);
 
-    expect(graph.verticies.getAt(0).component).toBeInstanceOf(Wire);
-    expect(graph.verticies.getAt(0).listOfConnections.getAt(0)).toEqual(1);
-    expect(graph.verticies.getAt(0).listOfConnections.getAt(1)).toEqual(2);
-    expect(graph.verticies.getAt(2).listOfConnections.getAt(0)).toEqual(0);
+    expect(graph.vertices.getAt(0).component).toBeInstanceOf(Wire);
+    expect(graph.vertices.getAt(0).listOfConnections.getAt(0)).toEqual(1);
+    expect(graph.vertices.getAt(0).listOfConnections.getAt(1)).toEqual(2);
+    expect(graph.vertices.getAt(2).listOfConnections.getAt(0)).toEqual(0);
 
     graph.removeVertex(0);
 
-    expect(graph.verticies.getAt(0).component).toBeInstanceOf(Battery);
-    expect(graph.verticies.getAt(1).listOfConnections.getAt(0)).toEqual(undefined);
+    expect(graph.vertices.getAt(0).component).toBeInstanceOf(Battery);
+    expect(graph.vertices.getAt(1).listOfConnections.getAt(0)).toEqual(undefined);
   });
 });
